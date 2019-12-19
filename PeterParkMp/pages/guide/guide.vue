d4337426a2f205a847b1300c5f3e468743b9b759
HaoZhe Qin <479754908@qq.com>
2019-12-19 21:45:33 +0800

    [add]guide page

---
+++ PeterParkMp/pages/guide/guide.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="bg-white guide">
+		<view class="guide-image">
+			<image src="../../static/img/navigaHL.png" mode="aspectFit"></image>
+		</view>
+		<view class="guide-text">
+			<text class="text-bold">PeterPark</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+			setTimeout(function() {
+				uni.switchTab({
+					url: '../main/main',
+					animationType: 'fade-in'
+				})
+			}, 2000);
+
+		}
+	}
+</script>
+
+<style scoped>
+	.guide{
+		min-height:100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		align-content: stretch;
+	}
+	.guide-image{
+		margin-top: 20vh;
+		flex: auto;
+	}
+	.guide-text{
+		flex: auto;
+		font-size: 60rpx;
+		color: #5CACEE;
+	}
+</style>
