4bfe873d91084d6fd09ff52124121eed17c6e3c1
赵铭心 <>
2019-12-20 13:36:05 +0800

    [mod]改成了顺眼一点的图标

--- PeterParkMp/pages/accountingRules/accountingRules.vue
+++ PeterParkMp/pages/accountingRules/accountingRules.vue
@@ -4,31 +4,31 @@
 			<text class="text-bold">停车场收费如下：</text>
 		</view>
 		<view class="rule-text">
-			<text class="text-bold cuIcon-check">\t</text>
+			<text class="text-bold cuIcon-favorfill">\t</text>
 			<text>2小时内，每小时10元</text>
 		</view>
 		<view class="rule-text">
-			<text class="text-bold cuIcon-check">\t</text>
+			<text class="text-bold cuIcon-favorfill">\t</text>
 			<text>超过2小时但小于5小时部分，每小时7元</text>
 		</view>
 		<view class="rule-text">
-			<text class="text-bold cuIcon-check">\t</text>
+			<text class="text-bold cuIcon-favorfill">\t</text>
 			<text>超过5小时的部分，每小时5元</text>
 		</view>
 		<view class="rule-text">
-			<text class="text-bold cuIcon-check">\t</text>
+			<text class="text-bold cuIcon-favorfill">\t</text>
 			<text>每日停车收费上限为41元</text>
 		</view>
 		<view class="rule-text">
-			<text class="text-bold cuIcon-check">\t</text>
+			<text class="text-bold cuIcon-favorfill">\t</text>
 			<text>每停车24h后，作为新的一天计算</text>
 		</view>
 		<view class="rule-text">
-			<text class="text-bold cuIcon-check">\t</text>
+			<text class="text-bold cuIcon-homefill">\t</text>
 			<text>最终解释权归PeterPark所有</text>
 		</view>
 		<view class="rule-text">
-			<text class="text-bold cuIcon-check">\t</text>
+			<text class="text-bold cuIcon-homefill">\t</text>
 			<text>祝您停车愉快！</text>
 		</view>
 	</view>
